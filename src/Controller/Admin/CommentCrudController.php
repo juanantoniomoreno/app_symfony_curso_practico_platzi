1cfb3046f3eae08b09d34504ab6ac664f784d9aa
Juan Antonio Moreno <ja.morenofernandez@gmail.com>
2023-04-12 22:26:42 +0200

    translation finished

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -20,6 +20,8 @@ class CommentCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
+        ->setEntityLabelInSingular('Comentario')
+        ->setEntityLabelInPlural('Comentarios')
             ->setSearchFields(['content'])
             ->setDefaultSort(['id' => 'DESC']);
     }
